77dd9b5881695e96cb37bc1e671c11f0bcd5162f
Nakyoung Jang <skrud4268@naver.com>
2021-11-27 15:07:07 +0900

    HTML : scroll fade up

--- main.js
+++ main.js
@@ -91,3 +91,27 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 };
+
+// Fade up as scroll
+function isElementUnderBottom(elem, triggerDiff) {
+    const { top } = elem.getBoundingClientRect();
+    const { innerHeight } = window;
+    return top > innerHeight + (triggerDiff || 0);
+  }
+  
+  function handleScroll() {
+    const elems = document.querySelectorAll('.scroll-up');
+    elems.forEach(elem => {
+      if (isElementUnderBottom(elem, -20)) {
+        elem.style.opacity = "0";
+        elem.style.transform = 'translateY(70px)';
+  
+      } else {
+        elem.style.opacity = "1";
+        elem.style.transform = 'translateY(0px)';
+  
+      }
+    })
+  }
+  
+  window.addEventListener('scroll', handleScroll);
